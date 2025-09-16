e6269f6947b6acf6361f570bd197fb7862247fc7
Criper98 <criper98n@gmail.com>
2025-09-16 18:49:51 +0200

    1.0.2 - Optimized color conversion (FlameHorizon) - Added display of image size - Added option to adjust saturation - Added mouse coordinates display when hovering over the output image - Fixed issue that caused incorrect saving of palette usage preferences

--- src/Wplace Palette Converter/Views/About.cs
+++ src/Wplace Palette Converter/Views/About.cs
@@ -20,36 +20,37 @@ namespace WplacePaletteConverter.Views
             lblVersion.Text = $"v{Application.ProductVersion}";
         }
 
-        private void lnkGitHub_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        private void OpenSite(string url)
         {
             System.Diagnostics.Process.Start(
-                new System.Diagnostics.ProcessStartInfo("https://github.com/Criper98/Wplace-Palette-Converter")
+                new System.Diagnostics.ProcessStartInfo(url)
                 { UseShellExecute = true }
             );
+        }
+
+        private void lnkGitHub_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            OpenSite("https://github.com/Criper98/Wplace-Palette-Converter");
         }
 
         private void lnkLicense_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
-            System.Diagnostics.Process.Start(
-                new System.Diagnostics.ProcessStartInfo("https://raw.githubusercontent.com/Criper98/Wplace-Palette-Converter/refs/heads/main/LICENSE")
-                { UseShellExecute = true }
-            );
+            OpenSite("https://raw.githubusercontent.com/Criper98/Wplace-Palette-Converter/refs/heads/main/LICENSE");
+        }
+
+        private void lnkFlameHorizon_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            OpenSite("https://github.com/FlameHorizon");
         }
 
         private void lnkNewtonsoftJson_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
-            System.Diagnostics.Process.Start(
-                new System.Diagnostics.ProcessStartInfo("https://raw.githubusercontent.com/JamesNK/Newtonsoft.Json/refs/heads/master/LICENSE.md")
-                { UseShellExecute = true }
-            );
+            OpenSite("https://raw.githubusercontent.com/JamesNK/Newtonsoft.Json/refs/heads/master/LICENSE.md");
         }
 
         private void lnkColourful_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
-            System.Diagnostics.Process.Start(
-                new System.Diagnostics.ProcessStartInfo("https://raw.githubusercontent.com/tompazourek/Colourful/refs/heads/master/LICENSE")
-                { UseShellExecute = true }
-            );
+            OpenSite("https://raw.githubusercontent.com/tompazourek/Colourful/refs/heads/master/LICENSE");
         }
     }
 }
